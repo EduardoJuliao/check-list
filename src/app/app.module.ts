3937f41be022aff5b6cca0999f7b7b2928364057
Eduardo Julião <eduardo.juliaojr@hotmail.com>
2018-03-23 15:41:50 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/** Perfect scrollbar */
+import { MalihuScrollbarModule } from 'ngx-malihu-scrollbar';
+
+/** message */
+import { Ng2IziToastModule } from 'ng2-izitoast';
+import { MessageService } from './../services/message-service';
+
+/** translate */
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'; 
+
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +22,23 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MalihuScrollbarModule.forRoot(),
+    Ng2IziToastModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+        },
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/services/message-service.ts
@@ -0,0 +1,86 @@
+import { TranslateService } from '@ngx-translate/core';
+import { Injectable } from '@angular/core';
+import { Ng2IzitoastService } from 'ng2-izitoast';
+
+@Injectable()
+export class MessageService {
+
+    constructor(public iziToast: Ng2IzitoastService,
+        private translate: TranslateService) {
+        iziToast.settings({
+            position: "topCenter",
+        })
+    }
+
+    public success(message: string): void {
+        this.translate.get('MESSAGE.SUCCESS').subscribe((res: string) => {
+            this.iziToast.success({
+                title: res,
+                message: message
+            });
+        });
+    }
+
+    public warning(message: string): void {
+        this.translate.get('MESSAGE.WARNING').subscribe((res: string) => {
+            this.iziToast.warning({
+                title: res,
+                message: message
+            });
+        });
+    }
+
+    public error(message: string): void {
+        this.translate.get('MESSAGE.ERROR').subscribe((res: string) => {
+            this.iziToast.error({
+                title: res,
+                message: message
+            });
+        });
+    }
+
+    public info(message: string): void {
+        this.translate.get('MESSAGE.INFO').subscribe((res: string) => {
+            this.iziToast.info({
+                title: res,
+                message: message
+            });
+        });
+    }
+
+    public ask(options: IMessageAsk): void {
+
+        this.translate.get('MESSAGE.WARNING').subscribe((res: string) => {
+            let yes = this.translate.instant("MESSAGE.YES");
+            let no = this.translate.instant("MESSAGE.NO");
+            this.iziToast.question({
+                title: options.title,
+                message: options.message,
+                timeout: 0,
+                buttons: [
+                    [`<button><b>${yes}</b></button>`, function (instance, toast) {
+                        if (options.onOk != undefined) {
+                            options.onOk();
+                        }
+                        instance.hide(toast,{ transitionOut: 'fadeOut' }, 'button');
+                    }],
+                    [`<button><b>${no}</b></button>`, function (instance, toast) {
+                        if (options.onCancel != undefined) {
+                            options.onCancel();
+                        }
+                        instance.hide(toast, { transitionOut: 'fadeOut' }, 'button');
+                    }]
+                ]
+            });
+        });
+
+
+    }
+}
+
+export interface IMessageAsk {
+    onOk?: () => void;
+    onCancel?: () => void;
+    title: string;
+    message: string;
+}
